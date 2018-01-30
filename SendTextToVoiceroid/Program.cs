dcd37bdbf847b73c767518b2b7c672242a57a421
Tachibana446 <stinger33z@gmail.com>
2018-01-31 04:47:37 +0900

    [add]voiceroidで文字列を読み上げさせる

---
+++ SendTextToVoiceroid/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SendTextToVoiceroid
+{
+    class Program
+    {
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool SetForegroundWindow(IntPtr hWnd);
+
+        static void Main(string[] args)
+        {
+
+            foreach (var p in System.Diagnostics.Process.GetProcesses())
+            {
+                if (Regex.IsMatch(p.ProcessName, @"VoiceroidEditor"))
+                {
+                    SetForegroundWindow(p.MainWindowHandle);
+                    SendKeys.SendWait("^a");
+                    SendKeys.SendWait("^v");
+                    SendKeys.SendWait("{F5}");
+                    break;
+                }
+            }
+        }
+    }
+}
