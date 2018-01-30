91a38ffdebe17c4fa91d2394750798ff15d346f0
Tachibana446 <stinger33z@gmail.com>
2018-01-31 06:10:30 +0900

    [add]完成

--- SendTextToVoiceroid/Program.cs
+++ SendTextToVoiceroid/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Runtime.InteropServices;
 using System.Text;
@@ -17,15 +18,30 @@ namespace SendTextToVoiceroid
 
         static void Main(string[] args)
         {
+            bool isFoundGameProcess = false;
+            bool isPlayedVoice = false;
+            Process game = null;
 
             foreach (var p in System.Diagnostics.Process.GetProcesses())
             {
-                if (Regex.IsMatch(p.ProcessName, @"VoiceroidEditor"))
+                if (!isFoundGameProcess && p.ProcessName == "program" && p.MainWindowTitle == "VoiceToText")
+                {
+                    game = p;
+                    isFoundGameProcess = true;
+                }
+
+                if (!isPlayedVoice && Regex.IsMatch(p.ProcessName, @"VoiceroidEditor"))
                 {
                     SetForegroundWindow(p.MainWindowHandle);
                     SendKeys.SendWait("^a");
                     SendKeys.SendWait("^v");
                     SendKeys.SendWait("{F5}");
+                    isPlayedVoice = true;
+                }
+
+                if (isFoundGameProcess && isPlayedVoice)
+                {
+                    SetForegroundWindow(game.MainWindowHandle);
                     break;
                 }
             }
